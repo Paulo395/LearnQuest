cf6b17062fa1399b2174a7df63ff2327c1098942
Paulo <paulinho395@gmail.com>
2024-04-17 20:38:47 -0300

    Atualizações Gerais

--- src/Pages/Aluno/AlunoSeminarios.js
+++ src/Pages/Aluno/AlunoSeminarios.js
@@ -23,15 +23,14 @@ const AlunoSeminarios = () => {
       {seminarios.map(seminario => (
         <div key={seminario.id}>
           <h3>{seminario.titulo}</h3>
-          <iframe 
-            width="560" 
-            height="315" 
-            src="https://www.youtube.com/embed/tsJXoqvmgzM?si=VFF8hYuIGZNMiIcw" 
-            title="YouTube video player" 
-            frameborder="0" 
-            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" 
-            referrerpolicy="strict-origin-when-cross-origin" 
-            allowfullscreen></iframe>
+          <iframe width="560" 
+          height="315" 
+          src="https://www.youtube.com/embed/FTQbiNvZqaY?si=w3nIFP5DMRQc58Lm" 
+          title="YouTube video player" 
+          frameborder="0" 
+          allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" 
+          referrerpolicy="strict-origin-when-cross-origin" 
+          allowfullscreen></iframe>
           <p>{seminario.descricao}</p>
         </div>
       ))}
